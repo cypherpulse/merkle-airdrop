56019e86ae0d607a76075a56745ca230423b0072
cypherpulse <johnbradill67@gmail.com>
2026-05-13 15:08:13 +0300

    Add DeployMerkleAirdrop script for deploying airdrop contract and minting tokens

---
+++ script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DeployMerkleAirdrop is Script {
+    bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private s_amountToTransfer = 4 * 25 * 1e18;
+
+    function run() external returns (MerkleAirdrop, BagelToken) {
+        return deployMerkleAirdrop();
+    }
+
+    function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
+        vm.startBroadcast();
+        BagelToken token = new BagelToken();
+        MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
+        // Mint tokens to the deployer (owner of the token contract by default)
+        token.mint(token.owner(), s_amountToTransfer);
+        // Transfer tokens from the deployer to the airdrop contract
+        token.transfer(address(airdrop), s_amountToTransfer);
+        vm.stopBroadcast();
+        return (airdrop, token);
+    }
+}
